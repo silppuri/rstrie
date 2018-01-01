cc495a69b08f66913011786f8095ed56666f39ef
Petri Avikainen <petri.avikainen@gmail.com>
2018-01-01 16:13:41 +0200

    use sequence trie

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
 #[macro_use]
 extern crate helix;
+extern crate sequence_trie;
+
+use sequence_trie::SequenceTrie;
 
 ruby! {
-    class Rstrie {
+    pub class Rstrie {
+        struct {
+            trie: SequenceTrie<String,bool>
+        }
+
+        def initialize(helix) {
+            Rstrie { helix, trie: SequenceTrie::<String, bool>::new() }
+        }
         def insert(value: String) -> String {
             println!("LOG: {:?}", value);
             value
